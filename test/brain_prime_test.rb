c3e500d4fcd0b14fe8fdf70da1018e5f0dd30d80
Sergey <95743381+Amanetes@users.noreply.github.com>
2022-05-23 14:03:13 +0300

    Update brain_prime_test.rb

--- test/brain_prime_test.rb
+++ test/brain_prime_test.rb
@@ -5,7 +5,7 @@ require_relative '../lib/games/brain_prime'
 require 'minitest/pride'
 
 class PrimeTest < Minitest::Test
-  def test_gcd
+  def test_prime
     assert_equal true, prime?(11)
     assert_equal false, prime?(52)
   end
